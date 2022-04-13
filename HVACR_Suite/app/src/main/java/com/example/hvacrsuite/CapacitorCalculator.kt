53fe64ac01ba280facd72fe24d5bc92a74ede811
Albert <albertgoodwin@ccimail.com>
2022-04-13 15:27:01 -0400

    Functional Math

--- HVACR_Suite/app/src/main/java/com/example/hvacrsuite/CapacitorCalculator.kt
+++ HVACR_Suite/app/src/main/java/com/example/hvacrsuite/CapacitorCalculator.kt
@@ -1,12 +1,26 @@
 package com.example.hvacrsuite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hvacrsuite.databinding.ActivityCapacitorCalculatorBinding
+
 
 class CapacitorCalculator : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCapacitorCalculatorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_capacitor_calculator)
+        binding = ActivityCapacitorCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        binding.button.setOnClickListener {
+            val volts = binding.voltage.text.toString().toFloat()
+            val amps = binding.amps.text.toString().toFloat()
+            val output = (amps * 2652) / volts
+            binding.result.text = output.toString()
+        }
     }
 }
